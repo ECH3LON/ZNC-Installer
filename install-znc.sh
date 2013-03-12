dc8f193eb86a18d029f8ddd1c5ac3343cbf43abc
ECH3LON <ech3lon.is.a.pirate@gmail.com>
2013-03-11 23:53:11 -0600

    Create install-znc.sh
    This very simplistic shell script will install the latest "stable" ZNC release from source. 
    
    So far this is only for Debian.

---
+++ install-znc.sh
@@ -0,0 +1,10 @@
+apt-get update
+wget http://znc.in/releases/znc-1.0.tar.gz
+tar -xzvf znc*.*gz
+cd znc*
+aptitude install build-essential -y
+aptitude install libssl-dev -y
+aptitude install libperl-dev -y
+./configure
+make
+make install
